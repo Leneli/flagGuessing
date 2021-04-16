a71ab7f134d8516138bfa383891575a04440c8c5
Elena Kuklina <liafell.lansera@gmail.com>
2021-04-16 09:15:28 +0300

    Initial Commit

---
+++ FlagGuessing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlagGuessing
+//
+//  Created by  Elena Kuklina on 16.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FlagGuessing/FlagGuessingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagGuessingApp.swift
+//  FlagGuessing
+//
+//  Created by  Elena Kuklina on 16.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct FlagGuessingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
